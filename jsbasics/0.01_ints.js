d81a97301e5093341a1b528db257a0041d83cd6c
jonathaneyster <jonathaneyster@Jonathans-MacBook-Pro.local>
2016-09-21 14:41:41 -0400

    worked on project

---
+++ jsbasics/0.01_ints.js
@@ -0,0 +1,9 @@
+var num=7;
+var newNum=5;//camel casing
+var newer_num=10; //this is snake casing
+
+//console.log() prints the console
+//operations with console.log
+console.log(5+5);
+//Modulus Operator
+console.log(10%5)
